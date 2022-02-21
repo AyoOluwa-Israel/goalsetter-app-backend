c95e609acb5ddb410c2a314033a295d94e499719
Adeleke Israel <izzyfresher@gmail.com>
2022-02-21 18:05:57 +0100

    fixed cors

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 // const path = require('path')
 const express = require("express");
+const cors = require('cors')
 const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
-const { json } = require("express");
+// const { json } = require("express");
+
+
 const port = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
+app.use(cors())
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
